ecf53a99055063a51a91f16f8330378e05c19422
annofy <zhenglfsir@gmail.com>
2017-01-10 23:21:16 +0800

    注册权限修改

--- routes/signup.js
+++ routes/signup.js
@@ -55,7 +55,7 @@ router.post('/', checkNotLogin, function(req, res, next) {
         bio: bio,
         avatar: avatar,
         code: code,
-        role: 'N'
+        role: 'normal'
     };
     UserModel.create(user).then(function(result) {
         // result为插入 数据库后的返回值
